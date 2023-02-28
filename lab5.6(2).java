acf21616794af59f4182ecea8cd448afcb7f0b84
valeralviv <126603888+valeralviv@users.noreply.github.com>
2023-02-28 19:30:47 +0200

    Create lab5.6(2).java

---
+++ lab5.6(2).java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 33;
+        int tens = number / 10;
+        int ones = number % 10;
+        int sum = tens + ones;
+        System.out.println("Сума: " + number + " дорівнює " + sum);
+    }
+}
