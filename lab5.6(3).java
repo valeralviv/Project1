4344412064a7840e4770517ff932f28faa560a5e
valeralviv <126603888+valeralviv@users.noreply.github.com>
2023-02-28 19:36:47 +0200

    Create lab5.6(3).java

---
+++ lab5.6(3).java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        double number = 3.654;
+        long roundedNumber = Math.round(number);
+        System.out.println("Округлене число: " + roundedNumber);
+    }
+}
